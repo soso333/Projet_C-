c9ad4f8dde1516d356e65cd9b877657ccd792382
soso333 <chouchou-kabylie@hotmail.com>
2026-04-17 16:40:11 +0200

    ajout de la classe aventurier et modification tresor

--- Tresor.cpp
+++ Tresor.cpp
@@ -1,7 +1,30 @@
-//classe ........
-
 class Tresor : public Case {
 
-    //blablablla
+    """ Classe permettant de définir le trésor qu'un joueur peut avoir
+
+    Paramètres : 
+        valeur : int (private)
+
+    Méthode : 
+        afficher() : char
+
+    """
+
+    private : 
+        int valeur;
+
+    public : 
+        //jsp 
+        void init(double a, double b){
+            x = a;
+            y = b;
+        }
+        void afficher(){
+            cout<<x<<"+i"<<y;
+        }
+
+        void appliqueEffet(){
+            
+        }
 
 };
