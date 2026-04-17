b2caa89d1a766f477b50d53f511d06e3dab9c4e3
Uxue Aizpurua <uxue8000@gmail.com>
2026-04-17 16:43:07 +0200

    Affichage cases

--- projet.cpp
+++ projet.cpp
@@ -5,8 +5,48 @@ using namespace std;
 
 class Case{
     public:
-        virtual char afficher() = ();
+        virtual char afficher() = 0;
         virtual ~Case() = default;
+};
+
+class Mur : public Case {
+    public:
+        char afficher(){
+            char a = '#';
+            return a;
+        }
+};
+
+class Passage : public Case{
+
+};
+class Tresor : public Case {
+    public:
+        char afficher(){
+            char a = '+';
+            return a;
+        }
+};
+class Monstre : public Case{
+    public:
+        char afficher(){
+            char a = 'M';
+            return a;
+        }
+};
+class Piege : public Case {
+    public:
+        char afficher(){
+            char a = 'T';
+            return a;
+        }
+};
+class Aventurier : public Case {
+    public:
+        char constafficher(){
+            char a = '@';
+            return a;
+        }
 };
 
 
